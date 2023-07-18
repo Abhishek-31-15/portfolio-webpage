1aa526752a52edacd4910ff877834a92e22dc7cb
Abhishek-31-15 <97472804+Abhishek-31-15@users.noreply.github.com>
2023-07-18 21:32:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,38 @@ for (var i = 0; i < navMenuAnchorTags.length; i++) {
 
 function scrollVertically(targetSection) {
     var targetSectionCoordinates = targetSection.getBoundingClientRect();
+     if (targetSectionCoordinates.top <= 0) {
+        clearInterval(interval);
+        return;
+    }
+    window.scrollBy(0, 50);
+
+var progressbars= document.querySelectorAll(".skill-progress > div");
+
+function initialiseBar(bar){ 
+bar.setAttribute("data-visited",false);
+bar.style.width = 0 + '%';
+}
+
+for (var bar of progressbars ){
+    initialiseBar(bar);
+}
+
+function fillBar(bar) {
+
+    var currentWidth = 0;
+    var targetWidth =  bar.getAttribute("data-bar-width");
+    var interval = setInterval(function() {
+        if (currentWidth >= targetWidth) {
+            clearInterval(interval);
+            return;
+        }
+        currentWidth++;
+        bar.style.width = currentWidth +'%';
+    },5);
+}
+
+progressbars.forEach(fillBar);   
     if (targetSectionCoordinates.top <= 0) {
         clearInterval(interval);
         return;
